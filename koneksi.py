455df5a8e58222e8615c14d96d2d9db6730bd2ab
Faris Wafda <farisjaguar2@gmail.com>
2024-12-18 14:44:30 +0700

    update

---
+++ koneksi.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+# Konfigurasi koneksi database
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'db_booking'
+}
+
+def get_db_connection():
+    """Membuat koneksi ke database."""
+    return mysql.connector.connect(**DB_CONFIG)
+
+if __name__ == '__main__':
+    app.run(debug=True)
